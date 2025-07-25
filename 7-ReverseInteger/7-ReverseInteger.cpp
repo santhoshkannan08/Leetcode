0eb76054a8816a38d5cae5632265e669bc656801
Santhoshkannan <148792759+santhoshkannan08@users.noreply.github.com>
2025-07-26 02:32:30 +0530

    Create 7-ReverseInteger

---
+++ 7-ReverseInteger/7-ReverseInteger.cpp
@@ -0,0 +1,17 @@
+// Last updated: 7/26/2025, 2:32:35 AM
+class Solution {
+public:
+    int reverse(int x) {
+        long long reversed=0;
+        while(x!=0){
+           
+            int digit=x%10;
+            reversed=(reversed*10)+digit;
+            x/=10;}
+            if (reversed < INT_MIN || reversed > INT_MAX) {
+            return 0;
+        }
+
+        return static_cast<int>(reversed);
+    } 
+};
