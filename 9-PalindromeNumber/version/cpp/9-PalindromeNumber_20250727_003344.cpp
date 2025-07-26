5200601c3f8a3ed7c6a494ee5331b4099c0dd18c
Santhoshkannan <148792759+santhoshkannan08@users.noreply.github.com>
2025-07-27 00:33:39 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/version/cpp/9-PalindromeNumber_20250727_003344.cpp
@@ -0,0 +1,17 @@
+// Last updated: 7/27/2025, 12:33:44 AM
+class Solution {
+public:
+    bool isPalindrome(long long x) {
+        long long dup=x;
+        long long lastdigit=0;
+        long long revnum=0;
+        while(x>0){
+            lastdigit=x%10;
+            revnum=(revnum*10)+lastdigit;
+            x/=10;
+        }
+        if(dup==revnum) return true;
+        else return false;
+        
+    }
+};
