86b05df3326e5baefa3c179e3a3b4f2ab7f679b8
Santhoshkannan <148792759+santhoshkannan08@users.noreply.github.com>
2025-07-29 00:02:08 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/version/cpp/125-ValidPalindrome_20250729_000211.cpp
@@ -0,0 +1,18 @@
+// Last updated: 7/29/2025, 12:02:11 AM
+class Solution {
+public:
+    bool isPalindrome(string& s, int i = 0) {
+        if (i == 0) {
+            string cleaned;
+            for (char c : s) {
+                if (isalnum(c)) cleaned += tolower(c);
+            }
+            s = cleaned;
+        }
+
+        int n = s.size();
+        if (i >= n / 2) return true;
+        if (s[i] != s[n - i - 1]) return false;
+        return isPalindrome(s, i + 1);
+    }
+};
