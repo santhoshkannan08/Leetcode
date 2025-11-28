6012a3ffc58dcc303ed4051703c1fbde034bcbe0
S Santhosh kannan <148792759+santhoshkannan08@users.noreply.github.com>
2025-11-29 02:04:44 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/version/java/125-ValidPalindrome_20251129_020424.java
@@ -0,0 +1,19 @@
+// Last updated: 11/29/2025, 2:04:24 AM
+1class Solution {
+2    public void reverseString(char[] s) {
+3        int left =0;
+4        int right= s.length-1;
+5        while(left<right){
+6            char temp;
+7            temp=s[left];
+8            s[left]=s[right];
+9            s[right]=temp;
+10            left++;
+11            right--;
+12
+13        }
+14
+15        
+16        
+17    }
+18}
